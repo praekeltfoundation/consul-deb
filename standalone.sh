e1fa9623de28a0dda700a013ef3fd379a5941e0c
Jamie Hewland <jhewland@gmail.com>
2015-12-14 15:56:34 +0200

    Try and improve the standalone script a bit

--- standalone.sh
+++ standalone.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -e
 
-# Standalone build file for building without Sideloader
+# Standalone build file for building without Sideloader. Expects to be run from
+# the root of the repo and will create a 'workspace' directory within the repo
+# in which to run the build.
+
+NAME="consul"
+VERSION="0.6.0"
+BUILDSCRIPT="build.sh"
+POSTINSTALL="postinstall.sh"
+
 export REPO_DIR="$(pwd)"
 
 # Create the workspace
@@ -9,12 +17,13 @@ cd workspace
 export WORKSPACE="$(pwd)"
 
 # Build
-$REPO_DIR/build.sh
+"$REPO_DIR/$BUILDSCRIPT"
 
-# Wrap the postinstall script
+# Wrap the postinstall script and make it executable
 echo "#!/bin/sh -e" > ./postinstall.sh
-cat $REPO_DIR/postinstall.sh >> ./postinstall.sh
+cat "$REPO_DIR/$POSTINSTALL" >> ./postinstall.sh
 chmod 0755 ./postinstall.sh
 
 # Run fpm
-fpm -s dir -t deb -a amd64 -n consul -v 0.6.0 --after-install $WORKSPACE/postinstall.sh --prefix / *
+cd package
+fpm -f -s dir -t deb -a amd64 -n $NAME -v $VERSION --after-install $WORKSPACE/postinstall.sh --prefix / *
